6036ad84726ff110fa4c38dd2aea87bba7a11437
GraceMwende <gracemwendem768@gmail.com>
2024-02-16 23:36:50 +0300

    change home icon to dashboard

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ header<- dashboardHeader(title = "STORE DATA")
 sidebar<-dashboardSidebar(
   sidebarMenu(
     #menuItem("Overview",tabName = "dashboard",icon = icon("dashboard")),
-    menuItem("Overview",tabName = "dashboard",icon = icon("house",class="fa-solid fa-house",lib='font-awesome')),
+    menuItem("Overview",tabName = "dashboard",icon = icon("dashboard")),
     menuItem("Products",tabName="products",icon = icon("send",lib='glyphicon')),
     menuItem("Customers",tabName="customers",icon = icon("user",lib='glyphicon'))
   )
