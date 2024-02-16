1e9fbe49b8efbdb754a5cb38512caa5c77db3a7c
GraceMwende <gracemwendem768@gmail.com>
2024-02-16 23:49:13 +0300

    change font awesome to verify false heroku deploy

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ header<- dashboardHeader(title = "STORE DATA")
 sidebar<-dashboardSidebar(
   sidebarMenu(
     #menuItem("Overview",tabName = "dashboard",icon = icon("dashboard")),
-    menuItem("Overview",tabName = "dashboard",icon = icon("dashboard")),
+    menuItem("Overview",tabName = "dashboard",icon = icon("dashboard",verify_fa = FALSE)),
     menuItem("Products",tabName="products",icon = icon("send",lib='glyphicon')),
     menuItem("Customers",tabName="customers",icon = icon("user",lib='glyphicon'))
   )
